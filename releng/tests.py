b66e2ad7e42aa80ca62660bae3275cff71ad948f
Jelle van der Waa <jelle@vdwaa.nl>
2017-06-11 18:57:25 +0200

    WIP

--- releng/tests.py
+++ releng/tests.py
@@ -1,7 +1,17 @@
+import hashlib
+from datetime import datetime
+
 from django.test import TestCase
 
+from releng.models import Release
+
+
 class RelengTest(TestCase):
+    fixtures = ['releng/fixtures/release.json']
 
     def test_feed(self):
         response = self.client.get('/feeds/releases/')
         self.assertEqual(response.status_code, 200)
+
+    def test_release(self):
+        print(Release.objects.all())
